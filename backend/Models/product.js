728956e542fc6a084f0ae9e089368aa2c8ac33e9
a5hwinjs <50653373+a5hwinjs@users.noreply.github.com>
2021-01-22 07:22:33 +0530

    Adding Product and Category Schemas in Model

---
+++ backend/Models/product.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+var productSchema = new mongoose.Schema({
+
+    name:{
+        type: String,
+        maxlength : 32,
+        trim : true,
+        required : true
+    },
+    description:{
+        type : String,
+        maxlength : 2000,
+        trim : true,
+        required : true
+    },
+    price:{
+        type: Number,
+        maxlength : 32,
+        trim : true,
+        required : true
+    },
+    category:{
+        type: ObjectId,
+        ref :"Category",
+        required :true
+    },
+    stock:{
+        type: Number
+    },
+    sold:{
+        type : Number,
+        default :0
+    },
+    photo:{
+        data : Buffer,
+        contentType : String
+    }
+},{timestamps : true})
+
+
+mongoose.exports = mongoose.model("Product",productSchema);
--- backend/Models/user.js
+++ backend/Models/user.js
@@ -1,47 +1,81 @@
-const { uniqueId } = require("lodash");
-const mongoose = require("mongoose");
-const { NIL } = require("uuid");
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const uuid = require('uuid/v1');
 
-    const userSchmea = new mongoose.Schema({
-        name:{
-            type:String,
-            required: true,
-            maxlength: 32,
-            trim:true
-        },
-        lastname:{
-            type: String,
-            maxlength: 32,
-            trim:true
-        },
-        email:{
-            type: String,
-            trim: true,
-            required: true,
-            unique: true
-        },
-        password:{
-            type: String,
-            trim: true 
 
-        },
-        salt: String,
-        role:{
+const userSchema = new mongoose.Schema({
+
+    name:{
+        type: String,
+        maxlength : 32,
+        required : true,
+        trim: true
+    },
+    lastname:{
+        type: String,
+        trim:true,
+        maxlength:32
+    },
+    email:{
+        type:String,
+        trim: true,
+        required: true,
+        unique : true
+    },
+    userinfo:{
+         type: String,
+         trim :  true
+    },
+    encryp_password:{
+        type: String,
+        trim: true,
+        required : true
+    },
+    salt: String,
+
+    role:{
             type: Number,
-            default:0
-        },
-        purchases:{
-            type: Array,
-            default: [],
+            default: 0
+    },
 
-        },
-        userinfo:{
-            type:String,
-            default:NIL,
-            trim:true
+    purchases :{
+        type: Array,
+        default :[]
+    }
+
+
+},{timestamps:true});
+
+userSchema.virtual("password").set(function(password){
+                    this._password = password,
+                    this.salt = uuid();
+                    this.encryp_password = this.securePassword(password);
+            }).get(function(){
+                return this._password;
+            })
+
+
+
+userSchema.method={
+
+    authenticate: function(plainpassword){
+        return this.securePassword(plainpassword)=== this.encryp_password
+
+    },
+    securePassword : function(plainpassword){
+        if(!password) return "";
+        try {
+            return  crypto.createHmac('sha256', this.salt)
+            .update(plainpassword)
+            .digest('hex');
+            
+        } catch (error) {
+            return "";
+            
         }
 
+    }
 
-        
-    })
-    module.exports=Mongoose.module("user",userSchmea);
+}
+module.exports= mongoose.model("user",userSchema);
+
