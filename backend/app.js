a8f2f5af1139b7408a78c79fde5a9bb9ce7b5f77
a5hwinjs <50653373+a5hwinjs@users.noreply.github.com>
2021-01-26 23:16:06 +0530

    adding dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
 const mongoose =require("mongoose");
 const express= require("express");
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/test",{useNewUrlParser:true});
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology : true,
+    useCreateIndex :true
+
+    }).then(()=>{
+        console.log("DB CONNECTED");
+    });
 
 const port  = 8000;
 
-app.listen(port,()=>)
+app.listen(port,()=>{
+    console.log(`The app is running in port ${port}`);
+});  
