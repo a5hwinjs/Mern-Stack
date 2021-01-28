f0813aa039d38db1cafd6b12a3c79d8b0633f2b0
a5hwinjs <50653373+a5hwinjs@users.noreply.github.com>
2021-01-28 23:51:36 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 const mongoose =require("mongoose");
 const express= require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser= require("cookie-parser");
+const cors =require("cors");
+
+
 
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
@@ -11,8 +16,11 @@ mongoose.connect(process.env.DATABASE,{
     }).then(()=>{
         console.log("DB CONNECTED");
     });
+app.use(bodyParser.json());
+app.use(cors());
+app.use(cookieParser());
 
-const port  = 8000;
+const port  = process.env.PORT || 8000;
 
 app.listen(port,()=>{
     console.log(`The app is running in port ${port}`);
